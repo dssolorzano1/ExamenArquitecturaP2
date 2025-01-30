ac4647835aed670f643d83be77beff8f1fbf4fbe
Deivy Steven Solorzano Briones <dssolorzano1@espe.edu.ec>
2025-01-29 20:05:58 -0500

    Parte 1

---
+++ branch-management/src/test/java/com/banquito/core/branch_management/BranchManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.branch_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
